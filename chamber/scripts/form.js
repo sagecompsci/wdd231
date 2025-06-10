dfc7f75fadc8b486d9cd44658db6261760a994b6
sagecompscie <sagereid32@gmail.com>
2025-06-10 17:26:02 -0600

    join

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,23 +1,23 @@
-const title = document.querySelector("#organization-title");
-const regEx = /[a-zA-Z- ]{7,}/;
-console.log("page load test");
-
-title.addEventListener('input', ()=> {
-    let correct = regEx.test(`${title.value}`);
-
-    if (!correct) {
-        title.setCustomValidity(false);
-    }
-
-    if (correct) {
-        title.setCustomValidity(true);
-    }
-    console.log(title.validity);
-});
+// const title = document.querySelector("#organization-title");
+// const regEx = /[a-zA-Z- ]{7,}/;
+// console.log("page load test");
+//
+// title.addEventListener('input', ()=> {
+//     let correct = regEx.test(`${title.value}`);
+//
+//     if (!correct) {
+//         title.setCustomValidity(false);
+//     }
+//
+//     if (correct) {
+//         title.setCustomValidity(true);
+//     }
+//     console.log(title.validity);
+// });
 
 const form = document.querySelector('.form1');
 const timestamp = document.querySelector('#time');
-form.addEventListener('submit', () => {
+document.addEventListener("DOMContentLoaded", () => {
     let date = new Date();
     timestamp.value = `${date.toLocaleTimeString()}   ${date.toLocaleDateString()}`;
 });
